3336072545ee2df385ea87d7e3db5b57641121f8
omar-moquete <moqueteperez@outlook.com>
2022-12-30 19:43:19 -0500

    updated

--- components/UI/buttons/SecondaryButton.jsx
+++ components/UI/buttons/SecondaryButton.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import classes from "./SecondaryButton.module.scss";
 
 const SecondaryButton = function (props) {
-  return (
-    <button {...props} className={`${classes.button} ${props.className}`}>
-      {props.children}
-    </button>
-  );
+  if (props.href)
+    return (
+      <a {...props} className={`${classes.link} ${props.className}`}>
+        {props.children}
+      </a>
+    );
+  else
+    return (
+      <button {...props} className={`${classes.button} ${props.className}`}>
+        {props.children}
+      </button>
+    );
 };
 
 export default SecondaryButton;
--- pages/404.jsx
+++ pages/404.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import PageNotFoundIllustration from "../components/UI/svg/NotFoundIllustration";
+import SecondaryButton from "../components/UI/buttons/SecondaryButton";
+import classes from "./404.module.scss";
 
 const NotFound = function () {
-  return <div>NotFound</div>;
+  return (
+    <div className={classes.main}>
+      <PageNotFoundIllustration />
+      <div>
+        <h2>404</h2>
+        <p>Sorry, the page you're looking for does not exist.</p>
+        <SecondaryButton href="./" className={classes.btn}>
+          Home page
+        </SecondaryButton>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
