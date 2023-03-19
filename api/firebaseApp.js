013f7beae271146479184aa87c0a931583f8f9e6
omar-moquete <moqueteperez@outlook.com>
2023-03-19 13:14:35 -0400

    updated

---
+++ api/firebaseApp.js
@@ -0,0 +1,19 @@
+// export const firebaseConfig = {
+//   apiKey: "AIzaSyBP6cRxnyDi4QcuqAmgOIBMJC32J2EUGqw",
+//   authDomain: "njsquestions-production.firebaseapp.com",
+//   projectId: "njsquestions-production",
+//   storageBucket: "njsquestions-production.appspot.com",
+//   messagingSenderId: "753600839103",
+//   appId: "1:753600839103:web:2250400337d7421a89750a",
+//   measurementId: "G-VLE26743MF",
+// };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAkip6mYWLvcnM6GXZYNSs49P9_Dtn5Z8o",
+  authDomain: "nextquestions.firebaseapp.com",
+  projectId: "nextquestions",
+  storageBucket: "nextquestions.appspot.com",
+  messagingSenderId: "214696588946",
+  appId: "1:214696588946:web:86e7070ff856a58421984b",
+  measurementId: "G-TJPX34TBY7",
+};
